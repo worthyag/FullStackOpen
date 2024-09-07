4c2b2e91a31ad06e9e27c21504860042d468adbf
Worthy <worthyintech@gmail.com>
2024-09-07 15:50:36 +0100

    Working on anecdotes.

--- part1/anecdotes/src/main.jsx
+++ part1/anecdotes/src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client"
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "./App.css"
+
+import App from "./App"
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(<StrictMode><App /></StrictMode>);
