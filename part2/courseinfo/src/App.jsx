b0a8077f0f59cee1795dfe13b7a350db39c601f8
Worthy <worthyintech@gmail.com>
2024-09-08 15:43:52 +0100

    Completed exercise 2.5.

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,25 +1,4 @@
-const Header = ( { course }) => <h1>{course}</h1>;
-
-const Part = ( { part, exercise }) => <p>{part} {exercise}</p>;
-
-const Content = ({ parts }) => 
-    parts.map(part => <Part key={part.id} part={part.name} exercise={part.exercises} />);
-
-const Total = ({ parts }) =>
-    <p className="total">
-      Total num of exercises {parts.reduce((sum, curr) => sum + curr.exercises, 0)}
-    </p>;
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
-};
-
+import Course from "./components/Course";
 
 const App = () => {
 
