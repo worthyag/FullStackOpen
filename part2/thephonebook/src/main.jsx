8aa7fff98a1e535d353b78fb837bde63d161d76f
Worthy <worthyintech@gmail.com>
2024-09-08 16:23:24 +0100

    Working on the phonebook.

---
+++ part2/thephonebook/src/main.jsx
@@ -0,0 +1,8 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+
+import "./App.css";
+
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<StrictMode><App /></StrictMode>);
