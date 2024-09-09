09183a09b3de993aa60599a676ba7fc103c313fa
Worthy <worthyintech@gmail.com>
2024-09-09 16:25:42 +0100

    Completed exercise 2.13.

--- part2/thephonebook/src/App.jsx
+++ part2/thephonebook/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
+
+import personService from "./services/persons";
 
 import Header from "./components/Header";
 import Filter from "./components/Filter";
@@ -14,10 +15,10 @@ const App = () => {
   const [filter, setFilter] = useState("");
 
   useEffect(() => {
-    axios
-      .get("http://localhost:3001/persons")
-      .then(response => {
-        setPersons(response.data);
+    personService
+      .getAll()
+      .then(initialPeople => {
+        setPersons(initialPeople);
       })
   });
 
@@ -35,10 +36,10 @@ const App = () => {
         number: newNumber
       };
 
-      axios
-        .post("http://localhost:3001/persons", personObj)
-        .then(response => {
-          setPersons(persons.concat(response.data));
+      personService
+        .create(personObj)
+        .then(returnedPerson => {
+          setPersons(persons.concat(returnedPerson));
           setNewName("");
           setNewNumber("");
         });    
