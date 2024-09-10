0822a28bc2cea59dcce16156e97c37bfcf8e1453
Worthy <worthyintech@gmail.com>
2024-09-10 13:35:56 +0100

    Working on exercise 2.18.

--- part2/dataforcountries/src/App.jsx
+++ part2/dataforcountries/src/App.jsx
@@ -1,8 +1,28 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 import FindCountry from "./components/FindCountry";
 
 const App = () => {
+  const [country, setCountry] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${country}`)
+      .then(response => {
+        console.log(response.data);
+      })
+  }, [country]);
+
+  const handleChange = (event) =>{
+    setCountry(event.target.value);
+  }
+
   return (
-    <FindCountry />
+    <FindCountry 
+      country={country} 
+      handleChange={handleChange}
+    />
   );
 };
 
--- part2/dataforcountries/src/components/FindCountry.jsx
+++ part2/dataforcountries/src/components/FindCountry.jsx
@@ -1,7 +1,16 @@
-const FindCountry = () => {
+const FindCountry = ({ country, handleChange }) => {
+  const styleObj = {
+    marginLeft: "12px"
+  }
+
   return (
     <p>
-      <span>find countries</span> <input />
+      <span>find countries</span> 
+      <input 
+        style={styleObj}
+        onChange={handleChange} 
+        value={country} 
+      />
     </p>
   );
 };
