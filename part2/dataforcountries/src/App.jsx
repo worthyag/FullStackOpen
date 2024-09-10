9767debb77096d7f50092b1701d5570f7e93a5e9
Worthy <worthyintech@gmail.com>
2024-09-10 14:34:50 +0100

    Working on exercise 2.18.

--- part2/dataforcountries/src/App.jsx
+++ part2/dataforcountries/src/App.jsx
@@ -5,17 +5,30 @@ import FindCountry from "./components/FindCountry";
 import DisplayCountryInfo from "./components/DisplayCountryInfo";
 
 const App = () => {
-  const [country, setCountry] = useState("");
+  const [country, setCountry] = useState("France");
+  const [displayedCountry, setDisplayedCountry] = useState({});
 
   useEffect(() => {
     axios
       .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${country}`)
       .then(response => {
         console.log(response.data);
+
+        const countryObj = {
+          name: response.data.name.common,
+          capital: response.data.capital,
+          area: response.data.area,
+          languages: Object.values(response.data.languages),
+          imgUrl: response.data.flags.png,
+          alt: response.data.flags.alt,
+        };
+
+        setDisplayedCountry(countryObj);
       })
   }, [country]);
 
   const handleChange = (event) =>{
+    setDisplayedCountry({})
     setCountry(event.target.value);
   }
 
@@ -25,7 +38,14 @@ const App = () => {
         country={country}
         handleChange={handleChange}
       />
-      <DisplayCountryInfo />
+      <DisplayCountryInfo
+        name={displayedCountry.name}
+        capitals={displayedCountry.capital}
+        area={displayedCountry.area}
+        languages={displayedCountry.languages}
+        imgUrl={displayedCountry.imgUrl}
+        alt={displayedCountry.alt}
+      />
     </div>
   );
 };
