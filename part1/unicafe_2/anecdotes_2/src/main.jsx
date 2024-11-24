c726b12963041fa40ca88cf98c9f51da638480c0
Worthy <worthyintech@gmail.com>
2024-11-24 21:23:00 +0000

    Finished the template.

--- part1/unicafe_2/anecdotes_2/src/main.jsx
+++ part1/unicafe_2/anecdotes_2/src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "./App.css";
+
+import App from "./App";
+
+
+ReactDOM.createRoot(document.querySelector("#root")).render(<StrictMode><App /></StrictMode>);
