4ec51cabe97826b1b57e7ad4519e42e7fb218d64
Worthy <worthyintech@gmail.com>
2024-11-24 14:12:56 +0000

    Moved the objects into an array.

--- part1/courseinfo_2/src/App.jsx
+++ part1/courseinfo_2/src/App.jsx
@@ -38,30 +38,32 @@ const Total = ({ total }) => {
 const App = () => {
   const course = "Half Stack application development";
 
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10
-  }
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7
-  }
-  const part3 = {
-    name: "State of a component",
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7
+    },
+    {
+      name: "State of a component",
+      exercises: 14
+    }
+  ];
 
 
   return (
     <div>
       <Header course={course} />
       <Content 
-        name1={part1.name} ex1={part1.exercises}
-        name2={part2.name} ex2={part2.exercises}
-        name3={part3.name} ex3={part3.exercises}
+        name1={parts[0].name} ex1={parts[0].exercises}
+        name2={parts[1].name} ex2={parts[1].exercises}
+        name3={parts[2].name} ex3={parts[2].exercises}
       />
       <Total 
-        total={part1.exercises + part2.exercises + part3.exercises}
+        total={parts[0].exercises + parts[1].exercises + parts[2].exercises}
       />
       
     </div>
