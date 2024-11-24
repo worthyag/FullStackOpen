954a2e41f88a326e17fcace51b99a346a0e71f13
Worthy <worthyintech@gmail.com>
2024-11-24 20:44:36 +0000

    Creating the template.

--- part1/unicafe_2/src/main.jsx
+++ part1/unicafe_2/src/main.jsx
@@ -1,10 +1,6 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from "react-dom/client";
+import "./App.css";
+import App from "./App.jsx";
+import { StrictMode } from "react";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.querySelector("#root")).render(<StrictMode><App /></StrictMode>);
