94907ece1c9121d96935d51496d944295aa08333
Worthy <worthyintech@gmail.com>
2024-11-25 08:51:34 +0000

    Implemented adding a person.

--- part2/thephonebook_2/src/App.jsx
+++ part2/thephonebook_2/src/App.jsx
@@ -1,24 +1,36 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas" }
-  ]);
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
+
+  const addPerson = (event) => {
+    event.preventDefault();
+
+    const personObj = {
+      name: newName
+    };
+    setPersons(persons.concat(personObj));
+    setNewName("");
+  };
+
+  const handleNewName = (event) => {
+    setNewName(event.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input onChange={handleNewName} value={newName} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person => <p key={person.name}>{person.name}</p>)}
     </div>
   );
 };
